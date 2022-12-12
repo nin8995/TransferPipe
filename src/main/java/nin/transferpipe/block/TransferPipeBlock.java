6c957af56c1305349fb6d3eaf0d58a55986dd3d3
nin8995 <toho.maru9.ggl2@gmail.com>
2022-12-12 23:05:19 +0900

    stick control

--- src/main/java/nin/transferpipe/block/TransferPipeBlock.java
+++ src/main/java/nin/transferpipe/block/TransferPipeBlock.java
@@ -2,42 +2,51 @@ package nin.transferpipe.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.Container;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.ChestBlock;
 import net.minecraft.world.level.block.PipeBlock;
-import net.minecraft.world.level.block.entity.ChestBlockEntity;
-import net.minecraft.world.level.block.entity.JukeboxBlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.material.Material;
-import net.minecraftforge.items.IItemHandler;
+import net.minecraft.world.phys.BlockHitResult;
 import nin.transferpipe.TransferPipe;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TransferPipeBlock extends PipeBlock {
 
     public TransferPipeBlock() {
         super(1 / 8F, BlockBehaviour.Properties.of(Material.STONE));
-        registerDefaultState(stateDefinition.any()
-                .setValue(UP, false)
-                .setValue(DOWN, false)
-                .setValue(NORTH, false)
-                .setValue(SOUTH, false)
-                .setValue(EAST, false)
-                .setValue(WEST, false)
-                .setValue(UP_ATTACHED, false)
-                .setValue(DOWN_ATTACHED, false)
-                .setValue(NORTH_ATTACHED, false)
-                .setValue(SOUTH_ATTACHED, false)
-                .setValue(EAST_ATTACHED, false)
-                .setValue(WEST_ATTACHED, false));
+        var s = stateDefinition.any();
+        for (Direction d : Direction.stream().toList())
+            s = s.setValue(CONNECTIONS.get(d), ConnectionStates.NONE);
+        s = s.setValue(FLOW, FlowStates.ALL);
+        registerDefaultState(s);
+    }
+
+    @Override
+    public InteractionResult use(BlockState bs, Level l, BlockPos pos, Player p, InteractionHand h, BlockHitResult p_60508_) {
+        if (p.getItemInHand(h).getItem() == Items.STICK)
+            l.setBlockAndUpdate(pos, bs.setValue(FLOW, FlowStates.getNext(l, pos, bs)));
+
+        return super.use(bs, l, pos, p, h, p_60508_);
     }
 
     public float getShadeBrightness(BlockState p_48731_, BlockGetter p_48732_, BlockPos p_48733_) {
@@ -57,10 +66,6 @@ public class TransferPipeBlock extends PipeBlock {
     @Override
     public void neighborChanged(BlockState bs, Level l, BlockPos bp, Block p_60512_, BlockPos p_60513_, boolean p_60514_) {
         super.neighborChanged(bs, l, bp, p_60512_, p_60513_, p_60514_);
-        neighborChanged(l, bp);
-    }
-
-    public void neighborChanged(Level l, BlockPos bp) {
         var os = l.getBlockState(bp);
         var ns = getState(l, bp);
         if (ns != os)
@@ -69,53 +74,113 @@ public class TransferPipeBlock extends PipeBlock {
 
     public BlockState getState(Level l, BlockPos p) {
         var s = defaultBlockState();
-        for (var d : Direction.stream().toList()) {
-            s = s.setValue(DtoDP(d), isPipe(l, p, d) || canConnect(l, p, d))
-                    .setValue(DtoDAP(d), canConnect(l, p, d));
-        }
+        for (Direction d : Direction.stream().toList())
+            s = s.setValue(CONNECTIONS.get(d), isPipe(l, p, d) && canGo(l,p,d) ? ConnectionStates.PIPE : canConnect(l, p, d) ? ConnectionStates.MACHINE : ConnectionStates.NONE);
+        if (l.getBlockState(p).hasProperty(FLOW))
+            s = s.setValue(FLOW, l.getBlockState(p).getValue(FLOW));//ここには初見さんも来る、BlockStateはあるがPropertyが定まってないヤツ
         return s;
     }
 
-    public boolean isPipe(LevelAccessor l, BlockPos p, Direction d) {
+    public static boolean isPipe(LevelAccessor l, BlockPos p, Direction d) {
         return l.getBlockState(p.relative(d)).getBlock() == TransferPipe.TRANSFER_PIPE.get();
+    }
+
+    public static boolean canGo(LevelAccessor l, BlockPos p, Direction d){
+        var me = l.getBlockState(p);
+        var you = l.getBlockState(p.relative(d));
+        return !(isOneWay(me, d) && isOneWay(you, d.getOpposite()));
+    }
+
+    public static boolean isOneWay(BlockState bs, Direction d){
+        var s = bs.getValue(FLOW);
+        return !(s == FlowStates.fromDirection(d) || s == FlowStates.ALL || s == FlowStates.IGNORE);
     }
 
     public boolean canConnect(LevelAccessor l, BlockPos p, Direction d) {
         return l.getBlockEntity(p.relative(d)) instanceof Container;
     }
 
-
-    public static final BooleanProperty NORTH_ATTACHED = BooleanProperty.create("north_attached");
-    public static final BooleanProperty EAST_ATTACHED = BooleanProperty.create("east_attached");
-    public static final BooleanProperty SOUTH_ATTACHED = BooleanProperty.create("south_attached");
-    public static final BooleanProperty WEST_ATTACHED = BooleanProperty.create("west_attached");
-    public static final BooleanProperty UP_ATTACHED = BooleanProperty.create("up_attached");
-    public static final BooleanProperty DOWN_ATTACHED = BooleanProperty.create("down_attached");
-
-    public static BooleanProperty DtoDP(Direction d) {
-        return switch (d) {
-            case DOWN -> DOWN;
-            case UP -> UP;
-            case NORTH -> NORTH;
-            case SOUTH -> SOUTH;
-            case WEST -> WEST;
-            case EAST -> EAST;
-        };
-    }
-
-    public static BooleanProperty DtoDAP(Direction d) {
-        return switch (d) {
-            case DOWN -> DOWN_ATTACHED;
-            case UP -> UP_ATTACHED;
-            case NORTH -> NORTH_ATTACHED;
-            case SOUTH -> SOUTH_ATTACHED;
-            case WEST -> WEST_ATTACHED;
-            case EAST -> EAST_ATTACHED;
-        };
-    }
+    public static final Map<Direction, EnumProperty<ConnectionStates>> CONNECTIONS = Direction.stream().collect(Collectors
+            .toMap(UnaryOperator.identity(), d -> EnumProperty.create(d.getName(), ConnectionStates.class)));
+    public static final EnumProperty<FlowStates> FLOW = EnumProperty.create("flow", FlowStates.class);
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(NORTH, EAST, SOUTH, WEST, UP, DOWN, NORTH_ATTACHED, EAST_ATTACHED, SOUTH_ATTACHED, WEST_ATTACHED, UP_ATTACHED, DOWN_ATTACHED);
+        CONNECTIONS.values().forEach(builder::add);
+        builder.add(FLOW);
+    }
+
+    @Override
+    protected int getAABBIndex(BlockState bs) {
+        int i = 0;
+        var ds = Direction.values();
+        for (int j = 0; j < ds.length; ++j)
+            if (bs.getValue(CONNECTIONS.get(ds[j])) != ConnectionStates.NONE
+                    && !(bs.getValue(CONNECTIONS.get(ds[j])) == ConnectionStates.MACHINE && bs.getValue(FLOW) == FlowStates.IGNORE))
+                i |= 1 << j;
+
+        return i;
+    }
+
+    public enum ConnectionStates implements StringRepresentable {
+        NONE,
+        PIPE,
+        MACHINE;
+
+        @Override
+        public String getSerializedName() {
+            return this.name().toLowerCase();
+        }
+    }
+
+    public enum FlowStates implements StringRepresentable {
+        ALL,
+        UP,
+        DOWN,
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST,
+        NONE,
+        IGNORE;
+
+        @Override
+        public String getSerializedName() {
+            return this.name().toLowerCase();
+        }
+
+        public static FlowStates fromDirection(Direction d) {
+            return Arrays.stream(FlowStates.values()).filter(f -> f.name().equals(d.name())).findFirst().get();
+        }
+
+        public static Stream<FlowStates> directions(){
+            return Direction.stream().map(FlowStates::fromDirection);
+        }
+
+        public static FlowStates[] statesWithout(FlowStates s){
+            return Arrays.stream(FlowStates.values()).filter(f -> f != s).toArray(FlowStates[]::new);
+        }
+
+        public FlowStates next() {
+            return Arrays.stream(FlowStates.values()).filter(fs -> fs.ordinal() == this.ordinal() + 1).findFirst().orElse(ALL);
+        }
+
+        public static FlowStates getNext(Level l, BlockPos p, BlockState bs) {
+            var validStates = new java.util.ArrayList<>(Arrays.stream(FlowStates.values()).toList());
+            var nonValidStates = new java.util.ArrayList<>(Direction.stream()
+                    .filter(d -> !isPipe(l, p, d)).map(FlowStates::fromDirection).toList());
+            if (nonValidStates.size() == 6) {
+                nonValidStates.add(NONE);
+            } else if(nonValidStates.size() == 5){
+                nonValidStates.add(ALL);
+            }
+            validStates.removeAll(nonValidStates);
+            var searching = bs.getValue(FLOW).next();
+            while (true) {
+                if (validStates.contains(searching))
+                    return searching;
+                searching = searching.next();
+            }
+        }
     }
 }
--- src/main/java/nin/transferpipe/data/TPBlockStateProvider.java
+++ src/main/java/nin/transferpipe/data/TPBlockStateProvider.java
@@ -1,15 +1,18 @@
 package nin.transferpipe.data;
 
+import net.minecraft.core.Direction;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraftforge.client.model.generators.ConfiguredModel;
 import net.minecraftforge.client.model.generators.ModelFile;
+import net.minecraftforge.client.model.generators.MultiPartBlockStateBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import nin.transferpipe.TransferPipe;
-import nin.transferpipe.block.TransferPipeBlock;
+
+import static nin.transferpipe.block.TransferPipeBlock.*;
+import static nin.transferpipe.block.TransferPipeBlock.ConnectionStates.MACHINE;
+import static nin.transferpipe.block.TransferPipeBlock.ConnectionStates.PIPE;
 
 public class TPBlockStateProvider extends net.minecraftforge.client.model.generators.BlockStateProvider {
 
@@ -26,24 +29,54 @@ public class TPBlockStateProvider extends net.minecraftforge.client.model.genera
         var block = ro.get();
         var id = ro.getId().getPath();
         var center = new ModelFile.UncheckedModelFile(modLoc("block/" + id));
+        var centerI = new ModelFile.UncheckedModelFile(modLoc("block/" + id + "_ignore"));
         var limb = new ModelFile.UncheckedModelFile(modLoc("block/" + id + "_limb"));
-        var attachment = new ModelFile.UncheckedModelFile(modLoc("block/" + id + "_joint"));
-        var inv = new ModelFile.UncheckedModelFile(modLoc("block/" + id + "_inv"));
+        var limbI = new ModelFile.UncheckedModelFile(modLoc("block/" + id + "_limb_ignore"));
+        var limbOW = new ModelFile.UncheckedModelFile(modLoc("block/" + id + "_limb_oneway"));
+        var joint = new ModelFile.UncheckedModelFile(modLoc("block/" + id + "_joint"));
+        var mb = getMultipartBuilder(block);
+        mb.part().modelFile(center).addModel()//通常中心
+                .condition(FLOW, FlowStates.statesWithout(FlowStates.IGNORE)).end();//無視したくないとき
 
-        getMultipartBuilder(block)
-                .part().modelFile(center).addModel().end()
-                .part().modelFile(limb).addModel().condition(BlockStateProperties.NORTH, true).end()
-                .part().modelFile(attachment).addModel().condition(TransferPipeBlock.NORTH_ATTACHED, true).end()
-                .part().modelFile(limb).rotationY(90).addModel().condition(BlockStateProperties.EAST, true).end()
-                .part().modelFile(attachment).rotationY(90).addModel().condition(TransferPipeBlock.EAST_ATTACHED, true).end()
-                .part().modelFile(limb).rotationY(180).addModel().condition(BlockStateProperties.SOUTH, true).end()
-                .part().modelFile(attachment).rotationY(180).addModel().condition(TransferPipeBlock.SOUTH_ATTACHED, true).end()
-                .part().modelFile(limb).rotationY(270).addModel().condition(BlockStateProperties.WEST, true).end()
-                .part().modelFile(attachment).rotationY(270).addModel().condition(TransferPipeBlock.WEST_ATTACHED, true).end()
-                .part().modelFile(limb).rotationX(90).addModel().condition(BlockStateProperties.DOWN, true).end()
-                .part().modelFile(attachment).rotationX(90).addModel().condition(TransferPipeBlock.DOWN_ATTACHED, true).end()
-                .part().modelFile(limb).rotationX(270).addModel().condition(BlockStateProperties.UP, true).end()
-                .part().modelFile(attachment).rotationX(270).addModel().condition(TransferPipeBlock.UP_ATTACHED, true).end();
+        mb.part().modelFile(centerI).addModel()//無視用中心
+                .condition(FLOW, FlowStates.IGNORE).end();//無視したいとき
+
+        Direction.stream().forEach(d -> {
+            var theWay = FlowStates.fromDirection(d);
+            var oneWayStates = new java.util.ArrayList<>(FlowStates.directions().filter(f -> f != theWay).toList());
+            oneWayStates.add(FlowStates.NONE);
+            rotate(mb.part().modelFile(limbOW), d).addModel()//一方通行用
+                    .condition(FLOW, oneWayStates.toArray(new FlowStates[]{}))//向いてる方向以外または塞ぎ込んでるとき
+                    .condition(CONNECTIONS.get(d), PIPE).end();//パイプに向けて
+
+            rotate(mb.part().modelFile(limb), d).addModel()//通常
+                    .condition(FLOW, theWay, FlowStates.ALL)//向いてる方向または開放的なとき
+                    .condition(CONNECTIONS.get(d), PIPE).end();//パイプに向けて
+            rotate(mb.part().modelFile(limb), d).addModel()//また
+                    .condition(FLOW, FlowStates.statesWithout(FlowStates.IGNORE))//無視したくないとき
+                    .condition(CONNECTIONS.get(d), MACHINE).end();//機械に向けて
+
+            rotate(mb.part().modelFile(joint), d).addModel()//接合部
+                    .condition(FLOW, FlowStates.statesWithout(FlowStates.IGNORE))//無視したくないとき
+                    .condition(CONNECTIONS.get(d), MACHINE).end();//機械に向けて
+
+            rotate(mb.part().modelFile(limbI), d).addModel()//無視用
+                    .condition(FLOW, FlowStates.IGNORE)//無視したいとき
+                    .condition(CONNECTIONS.get(d), PIPE).end();//パイプに向けて
+        });
+
+        var inv = new ModelFile.UncheckedModelFile(modLoc("block/" + id + "_inv"));
         simpleBlockItem(block, inv);
+    }
+
+    public static ConfiguredModel.Builder<MultiPartBlockStateBuilder.PartBuilder> rotate(ConfiguredModel.Builder<MultiPartBlockStateBuilder.PartBuilder> m, Direction d) {
+        return switch (d) {
+            case DOWN -> m.rotationX(90);
+            case UP -> m.rotationX(270);
+            case NORTH -> m.rotationY(0);
+            case SOUTH -> m.rotationY(180);
+            case WEST -> m.rotationY(270);
+            case EAST -> m.rotationY(90);
+        };
     }
 }
